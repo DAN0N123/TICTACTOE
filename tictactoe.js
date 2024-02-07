875cd153f17af1c50d1b9360e7dcd06710386d86
DAN0N123 <filipskubis01@wp.pl>
2024-02-07 23:06:35 +0100

    Finish tictactoe #essa

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,5 @@
 const board = document.querySelector('.board');
+const bigbox = document.querySelector('.bigbox');
 let move = 0;
 
 
@@ -27,7 +28,8 @@ const gameboard = (function() {
         boardArr.push(tileObj)
         
     }
-    const printBoard = function(){
+    const drawBoard = function(){
+        drawScoreboard()
         boardArr.forEach((tile) => {
             switch(tile.symbol){
                 case 'X':
@@ -60,7 +62,7 @@ const gameboard = (function() {
         const winningCombinations = [
             [0,1,2], [3,4,5], [6,7,8],
             [0,3,6], [1,4,7], [2,5,8],
-            [1,4,8], [2,4,6]
+            [0,4,8], [2,4,6]
         ]
         winningCombinations.forEach(function(combination){
             [a,b,c] = combination;
@@ -68,41 +70,118 @@ const gameboard = (function() {
                 boardArr[b].symbol === boardArr[c].symbol &&
                 boardArr[a].symbol !== null){
                 const winningPlayer = move % 2 === 0 ? player2.playerName : player1.playerName;
-                const line = document.createElement('div');
-                const max_row = Math.floor((Math.max(a,b,c) + 1) / 3) + 1;
-                const min_row = Math.floor((Math.min(a,b,c) + 1) / 3) + 2;
-                const min_col = Math.min(a, b, c) % 3 + 1;
-                const max_col = Math.max(a, b, c) % 3 + 2;
-                line.classList.add('line')
-                line.style.cssText = `background-color: ${winningPlayer === player1.playerName ? 'red':'blue'}; 
-                                      position: absolute;
-                                      height: 20px;
-                                      width: calc(100% - 3px);`
+                
+                const gridContainerWidth = board.offsetWidth;
 
-                line.style.gridRow = `${min_row} / ${max_row}`;
-                line.style.gridColumn = `${min_col} / ${max_col}`;
-                board.appendChild(line)
-                console.log(`${winningPlayer} has won!`)
+                const startRow = Math.min(Math.floor(a / 3), Math.floor(b / 3), Math.floor(c / 3));
+                const endRow = Math.max(Math.floor(a / 3), Math.floor(b / 3), Math.floor(c / 3)) + 1;
+                const startCol = Math.min(a % 3, b % 3, c % 3);
+                const endCol = Math.max(a % 3, b % 3, c % 3) + 1;
+
+                const numRows = 3;
+                const numColumns = 3;
+                const cellWidth = gridContainerWidth / numColumns;
+                const cellHeight = gridContainerWidth / numRows;
+                
+                const middleX = cellWidth / 2;
+                const middleY = cellHeight / 2;
+
+                const x1 = startCol * cellWidth + middleX;
+                const y1 = startRow * cellHeight + middleY;
+                const x2 = (endCol + 1) * cellWidth - middleX*3;
+                const y2 = (endRow + 1) * cellHeight - middleY*3; 
+
+
+                const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                svg.setAttribute("width", "100%");
+                svg.setAttribute("height", "100%");
+                const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+                line.classList.add('line')
+                line.setAttribute("x1", x1);
+                line.setAttribute("y1", y1);
+                line.setAttribute("x2", x2);
+                line.setAttribute("y2", y2);
+                // line.setAttribute("stroke", winningPlayer === player1.playerName ? '#FF615E' : '#3EC5F3');
+                line.setAttribute("stroke", 'black')
+                line.setAttribute("stroke-width", "20");
+
+                svg.appendChild(line);
+                board.appendChild(svg);
+                if(player1.playerName === winningPlayer){
+                    player1.addScore()
+                }else{ player2.addScore() }
+                drawScoreboard()
+                showWinScreen(winningPlayer)
             }
         })
     }
-
+    const drawScoreboard = function(){
+        const scoreboard = document.querySelector('.scoreboard');
+        scoreboard.innerHTML = '';
+        const players = document.createElement('h2');
+        players.textContent = `${player1.playerName} : ${player2.playerName}`;
+        scoreboard.appendChild(players);
+        const score = document.createElement('h1');
+        score.textContent = `${player1.score} : ${player2.score}`;
+        scoreboard.appendChild(score);
+    }
     const resetBoard = function(){
         board.innerHTML = '';
+    }
+    const resetGame = function(){
+        resetBoard();
+        boardArr.forEach( (tile) => tile.symbol = null);
+        drawBoard();
+        move = 0;
+        displayController.removeWinScreen()
+    }
+    const showWinScreen = function(winningPlayer){
+        boardArr.forEach( (tile) => {
+            const tileElement = document.getElementById(tile.id)
+            if (tile.symbol === null){
+                tileElement.removeEventListener('click', markTile)}});
+        const winScreen = document.createElement('div');
+        winScreen.classList.add('winscreen')
+        const winText = document.createElement('h2');
+        winText.textContent = `${winningPlayer} has won!`;
+        const buttonDiv = document.createElement('div');
+        buttonDiv.classList.add('buttons')
+        const restartButton = document.createElement('button');
+        restartButton.addEventListener('click', resetGame)
+        restartButton.textContent = 'PLAY AGAIN';
+        const newGameButton = document.createElement('button');
+        newGameButton.addEventListener('click', displayController.newGame)
+        newGameButton.textContent = 'NEW GAME';
+        buttonDiv.appendChild(restartButton);
+        buttonDiv.appendChild(newGameButton);
+        winScreen.appendChild(winText);
+        winScreen.appendChild(buttonDiv);
+        bigbox.appendChild(winScreen);
     }
     const markTile = function(which, symbol){
         boardArr[which].symbol = symbol;
         resetBoard();
-        printBoard();
+        drawBoard();
         move++;
         checkWin();
     }
-    return { board, printBoard, markTile, resetBoard}
+    return { board, drawBoard, markTile, resetBoard, resetGame, drawScoreboard}
 })();
 
 const displayController = (function() {
-    return { }
-})
+    const removeWinScreen = function(){
+        const winScreen = document.querySelector('.winscreen');
+        bigbox.removeChild(winScreen);
+    }
+    const newGame = function(){
+        console.log('yo');
+        gameboard.resetGame();
+        player1 = makePlayer(prompt('Provide name for player one: '));
+        player2 = makePlayer(prompt('Provide name for player two: '));
+        gameboard.drawScoreboard();
+    };
+    return { newGame, removeWinScreen};
+})();
 
 
 
@@ -112,11 +191,5 @@ const displayController = (function() {
 
 
 
-const scoreboard = document.querySelector('.scoreboard')
-const players = document.createElement('h2')
-players.textContent = `${player1.playerName} : ${player2.playerName}`
-scoreboard.appendChild(players)
-const score = document.createElement('h1');
-score.textContent = `${player1.score} : ${player2.score}`
-scoreboard.appendChild(score)
-gameboard.printBoard()
+
+gameboard.drawBoard()
